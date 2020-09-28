4975733b54f128d5ba23d544b2d72476e46887f6
Mirza Younus Baig <v-mirzba@microsoft.com>
2020-09-28 21:32:36 +0530

    Files for the test web api (contains errors)

---
+++ Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using Microsoft.AspNetCore.Builder;
+
+
+namespace TestWebAPI.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context => { 
+                context.Response
+                })
+            });
+        }
+    }
+}
---
+++ Models/ErrorDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebAPI.Models
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+
+    }
+}
